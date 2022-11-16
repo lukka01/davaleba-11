ddc6656a08077c4b0cbe446570344e598f929def
lukka01 <115227610+lukka01@users.noreply.github.com>
2022-11-17 00:30:44 +0400

    Add files via upload

---
+++ 11th workshop/js/nav.js
@@ -0,0 +1,20 @@
+const nav = ["home", "about", "gallery", "contact"];
+
+const links = ["index.html" , "about.html" ,"gallery.html" , "contacts.html"];
+
+nav.push("delivery");
+links.push("delivery.html");
+
+let navtext = '<ul class="flex">';
+
+
+for(let i =0; i<nav.length; i++){
+    navtext+= '<li><a href="'+ links[i] +'">' + nav[i] + "</a></li>";
+    console.log(navtext);
+}
+
+navtext+= "</ul>";
+console.log(navtext);
+
+document.getElementById("nav").innerHTML= navtext;
+
